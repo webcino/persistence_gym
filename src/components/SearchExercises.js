343c1f7757e3e76077a8231ba1b6ba654ccb15ec
Alcino_Namba <99806562+webcino@users.noreply.github.com>
2023-11-09 18:49:04 +0200

    Finished , still figuring out bugs

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,11 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { fetchData, exerciseOptions } from "../utils/fetchData";
+import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SearchExercises = () => {
+const SearchExercises = (setExercises, bodyPart, setBodyPart) => {
   const [search, setSearch] = useState("");
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      );
+      setBodyParts(["all", ...bodyPartsData]);
+    };
+    fetchExercisesData();
+  }, []);
+
   const handleSearch = async () => {
     if (search) {
-      // const exerciseData = await fetchData();
+      const exerciseData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exerciseOptions
+      );
+
+      const searchedExercises = exerciseData.filter((exercise) => {
+        exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase(search) ||
+          exercise.equipment.toLowerCase(search) ||
+          exercise.bodyPart.toLowerCase(search);
+      });
+
+      setSearch("");
+      setExercises(searchedExercises);
     }
   };
 
@@ -49,6 +77,13 @@ const SearchExercises = () => {
         >
           Search
         </Button>
+      </Box>
+      <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
       </Box>
     </Stack>
   );
